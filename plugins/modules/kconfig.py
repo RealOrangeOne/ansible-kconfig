2257b87dbe7e61e96d135c8fc101d599fd1a1936
Jake Howard <git@theorangeone.net>
2023-01-18 08:51:37 +0000

    Add the ability to write config

--- plugins/modules/kconfig.py
+++ plugins/modules/kconfig.py
@@ -33,11 +33,39 @@ class KConfigWrapper:
 
         return value
 
+    def write(self, file: Optional[str], group: str, key: str, value: str) -> bool:
+        # If no change is needed (or won't be done due to check_mode), notify
+        # caller straight away.
+        if value == self.read(file, group, key):
+            return False
+        elif self.check_mode:
+            return True
+
+        command = [self.write_config_bin, "--group", group, "--key", key]
+
+        if file is not None:
+            command.extend(["--file", file])
+
+        # The value must always be the last argument
+        command.append(value)
+
+        # Run the command and fetch standard return code, stdout, and stderr.
+        rc, out, err = self.module.run_command(command)
+
+        if rc != 0:
+            self.module.fail_json(
+                msg="kwriteconfig5 failed while write the value with error: %s" % err,
+                out=out,
+                err=err,
+            )
+
+        return True
+
 
 def main() -> None:
     module = AnsibleModule(
         argument_spec={
-            "state": {"default": "read", "choices": ["read"]},
+            "state": {"default": "present", "choices": ["present", "read"]},
             "key": {
                 "required": True,
                 "type": "str",
@@ -52,6 +80,10 @@ def main() -> None:
         supports_check_mode=True,
     )
 
+    # If present state was specified, value must be provided.
+    if module.params["state"] == "present" and module.params["value"] is None:
+        module.fail_json(msg='State "present" requires "value" to be set.')
+
     kconfig = KConfigWrapper(module)
 
     params = module.params
@@ -59,6 +91,11 @@ def main() -> None:
     if params["state"] == "read":
         value = kconfig.read(params["file"], params["group"], params["key"])
         module.exit_json(changed=False, value=value)
+    elif params["state"] == "write":
+        changed = kconfig.write(
+            params["file"], params["group"], params["key"], params["value"]
+        )
+        module.exit_json(changed=changed)
 
 
 if __name__ == "__main__":
